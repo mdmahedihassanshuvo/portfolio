82a0c63cdfeabe494f8c9fc26e3fa76f6b69f4c1
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2024-01-30 11:50:30 +0600

    change something in portfolio page

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import porject1 from "../../assets/Project/bistro.png";
 import porject2 from "../../assets/Project/sport.png";
+import porject3 from "../../assets/Project/furniro.png";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { FaGithub } from "react-icons/fa6";
+import { LuLink } from "react-icons/lu";
+
+import "./Portfolio.css";
 
 const Portfolio = () => {
   return (
-    <div className="md:py-[40px] p-5 md:mx-[100px]">
+    <div className="md:py-[40px] p-5 md:px-[100px] bg-[#f3f3f3]">
       <Helmet title="MH - Portfolio" />
       <div className="flex justify-between items-center md:mb-0 mb-5">
-        <h2 className='md:text-5xl text-2xl font-bold'>My Portfolio</h2>
+        <h2 className="md:text-5xl text-2xl font-bold">My Portfolio</h2>
         <Link
           target="black"
           to="https://github.com/mdmahedihassanshuvo"
@@ -33,93 +38,60 @@ const Portfolio = () => {
         </Link>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-10 md:mt-[100px]">
-        <div className="text-white card w-[350px] border overflow-hidden relative transition ease-in duration-200 transform hover:-translate-y-2 rounded-2xl shadow-2xl hover:shadow-2xl">
-          <figure>
-            <img className=" w-full p-2" src={porject1} alt="Bistro-boss" />
-          </figure>
-          <div className="card-body w-full h-full rounded-2xl  absolute bg-black bg-opacity-60 opacity-0 hover:opacity-100 inset-0 transition-opacity duration-200">
-            <h2 className="card-title font-bold">
-              BistroBoss
-              <Link
-                target="blank"
-                to="https://bistro-60b2f.web.app/"
-                className="badge badge-secondary"
-              >
-                Live Link
-              </Link>
-            </h2>
-            <p className="text-lg">If you interested view source code.</p>
-            <div className="card-actions justify-end">
-              <Link
-                target="blank"
-                to="https://github.com/mdmahedihassanshuvo/bistro-client"
-                className="badge badge-secondary"
-              >
-                Client
-              </Link>
-              <Link
-                target="blank"
-                to="https://github.com/mdmahedihassanshuvo/bistro-server"
-                className="badge badge-secondary"
-              >
-                Server
-              </Link>
+        <div className="bg-white rounded-lg">
+          <div className="coverdiv text-white overflow-hidden ">
+            <div className="md:w-full w-[350px] md:h-[250px] overflow-hidden relative cursor-pointer">
+              <img
+                className="portfolio-image w-full md:p-4  absolute left-0 right-0 top-0"
+                src={porject1}
+                alt="Bistro-boss"
+              />
+            </div>
+          </div>
+          <div className="bg-[#f7f7f7] md:m-4 p-4">
+            <h3 className="text-2xl font-semibold ">Bistro Boss Restaurant</h3>
+            <p className="text-sm text-slate-400 mt-2">E-commerce website</p>
+            <div className="flex justify-between md:mt-3 items-center">
+              <Link target="blank" to="https://github.com/mdmahedihassanshuvo/bistro-client" className="flex items-center gap-2 bg-blue-600 hover:bg-blue-800 rounded-lg p-2 text-white text-base font-medium">Sourch code <FaGithub /></Link>
+              <Link target="blank" to="https://bistro-60b2f.web.app/" className="flex items-center gap-2 bg-blue-600 hover:bg-blue-800 rounded-lg p-2 text-white text-base font-medium">Live Link <LuLink /></Link>
             </div>
           </div>
         </div>
-        <div className="text-white card w-[350px] border overflow-hidden relative transition ease-in duration-200 transform hover:-translate-y-2 rounded-2xl shadow-2xl hover:shadow-2xl">
-          <figure>
-            <img className=" w-full p-2" src={porject2} alt="Bistro-boss" />
-          </figure>
-          <div className="card-body w-full h-full rounded-2xl  absolute bg-black bg-opacity-60 opacity-0 hover:opacity-100 inset-0 transition-opacity duration-200">
-            <h2 className="card-title font-bold">
-              Sports Mania
-              <Link
-                to="https://sportsmania-f05ea.web.app/"
-                target="blank"
-                className="badge badge-secondary"
-              >
-                Live Link
-              </Link>
-            </h2>
-            <p className="text-lg">If you interested view source code.</p>
-            <div className="card-actions justify-end">
-              <Link
-                to="https://github.com/mdmahedihassanshuvo/SportsMania"
-                target="blank"
-                className="badge badge-secondary"
-              >
-                Client
-              </Link>
-              <Link
-                to="https://github.com/mdmahedihassanshuvo/SportsMania-server"
-                target="blank"
-                className="badge badge-secondary"
-              >
-                Server
-              </Link>
+        <div className="bg-white rounded-lg">
+          <div className="coverdiv text-white overflow-hidden ">
+            <div className="md:w-full w-[350px] md:h-[250px] overflow-hidden relative cursor-pointer">
+              <img
+                className="portfolio-image w-full md:p-4  absolute left-0 right-0 top-0"
+                src={porject2}
+                alt="Bistro-boss"
+              />
+            </div>
+          </div>
+          <div className="bg-[#f7f7f7] md:m-4 p-4">
+            <h3 className="text-2xl font-semibold ">Sports Mania</h3>
+            <p className="text-sm text-slate-400 mt-2">School management website</p>
+            <div className="flex justify-between md:mt-3 items-center">
+              <Link target="blank" to="https://github.com/mdmahedihassanshuvo/SportsMania" className="flex items-center gap-2 bg-blue-600 hover:bg-blue-800 rounded-lg p-2 text-white text-base font-medium">Sourch code <FaGithub /></Link>
+              <Link target="blank" to="https://sportsmania-f05ea.web.app/" className="flex items-center gap-2 bg-blue-600 hover:bg-blue-800 rounded-lg p-2 text-white text-base font-medium">Live Link <LuLink /></Link>
             </div>
           </div>
         </div>
-        <div className="text-white card w-[350px] border overflow-hidden relative transition ease-in duration-200 transform hover:-translate-y-2 rounded-2xl shadow-2xl hover:shadow-2xl">
-          <figure>
-            <img className=" w-full p-2" src={porject2} alt="Bistro-boss" />
-          </figure>
-          <div className="card-body w-full h-full rounded-2xl  absolute bg-black bg-opacity-60 opacity-0 hover:opacity-100 inset-0 transition-opacity duration-200">
-            <h2 className="card-title font-bold">
-              Sports Mania
-              <Link target="blank" className="badge badge-secondary">
-                Live Link
-              </Link>
-            </h2>
-            <p className="text-lg">If you interested view source code.</p>
-            <div className="card-actions justify-end">
-              <Link target="blank" className="badge badge-secondary">
-                Client
-              </Link>
-              <Link target="blank" className="badge badge-secondary">
-                Server
-              </Link>
+        <div className="bg-white rounded-lg">
+          <div className="coverdiv text-white overflow-hidden ">
+            <div className="md:w-full w-[350px] md:h-[250px] overflow-hidden relative cursor-pointer">
+              <img
+                className="portfolio-image w-full md:p-4  absolute left-0 right-0 top-0"
+                src={porject3}
+                alt="Bistro-boss"
+              />
+            </div>
+          </div>
+          <div className="bg-[#f7f7f7] md:m-4 p-4">
+            <h3 className="text-2xl font-semibold ">Furniro</h3>
+            <p className="text-sm text-slate-400 mt-2">E-commerce website</p>
+            <div className="flex justify-between md:mt-3 items-center">
+              <Link target="blank" to="https://github.com/mdmahedihassanshuvo/furniro-client" className="flex items-center gap-2 bg-blue-600 hover:bg-blue-800 rounded-lg p-2 text-white text-base font-medium">Sourch code <FaGithub /></Link>
+              <Link target="blank" to="https://fastidious-cuchufli-17d13f.netlify.app/" className="flex items-center gap-2 bg-blue-600 hover:bg-blue-800 rounded-lg p-2 text-white text-base font-medium">Live Link <LuLink /></Link>
             </div>
           </div>
         </div>
