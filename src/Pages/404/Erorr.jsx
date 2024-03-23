7c26d8a34140a1161695ba62bdca0d9712605f3c
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2024-03-23 11:45:41 +0600

    erorr page update

--- src/Pages/404/Erorr.jsx
+++ src/Pages/404/Erorr.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { FaceFrownIcon } from '@heroicons/react/24/solid'
-import { useRouteError } from 'react-router-dom';
+import { FaceFrownIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-    const error = useRouteError()
-    console.log(error)
-    const {status, statusText, data} = error
-    return (
-        <div className='flex flex-col justify-center gap-1 items-center min-h-screen'>
-            <div><FaceFrownIcon className="h-16 w-16 text-blue-500" /></div>
-            <h2 className='md:text-6xl text-3xl font-bold text-red-600'>{status}</h2>
-            <h2 className='md:text-6xl text-3xl'>{statusText}</h2>
-            <p className='md:text-xl text-base underline cursor-not-allowed'>{data}</p>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center gap-1 items-center min-h-screen">
+      <div>
+        <FaceFrownIcon className="h-16 w-16 text-blue-500" />
+      </div>
+      <h2 className="md:text-6xl text-3xl font-bold text-red-600">404</h2>
+      <h2 className="md:text-6xl text-3xl">Not Found</h2>
+      <Link
+        to="/"
+        className="md:text-xl text-blue-600 underline cursor-pointer"
+      >
+        Back To Home
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
