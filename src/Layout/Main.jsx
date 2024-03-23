15814f90150d099164a71c1cdd172c1608956efc
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2024-03-23 13:07:54 +0600

    change

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import Footer from "../Shared/Footer/Footer";
 import Loading from "../Pages/Loading/Loading";
-import { useState } from "react";
 // import "./Main.css";
 
 const Main = () => {
-  const [load, setLoad] = useState(true);
+  const navigation = useNavigation();
 
-  if (load) {
+  if (navigation.state === "loading") {
     return <Loading />;
   }
 
